2bb70b6a3b30a59bd9374d49fe4a028899cc4d60
Dmitry Zmerzlov <zmerzlovd@gmail.com>
2025-08-31 11:57:27 +0300

    Revert "New version"
    This reverts commit b5ee2ab7b625aa0c98215b33460cffe3efb0917f.

--- math/adding.go
+++ math/adding.go
@@ -1,7 +1,5 @@
-// Math provides Add function to add two number
 package math
 
-// Add function combine two number. See also https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
