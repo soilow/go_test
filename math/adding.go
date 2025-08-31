b5ee2ab7b625aa0c98215b33460cffe3efb0917f
Dmitry Zmerzlov <zmerzlovd@gmail.com>
2025-08-31 11:56:03 +0300

    New version

--- math/adding.go
+++ math/adding.go
@@ -1,5 +1,7 @@
+// Math provides Add function to add two number
 package math
 
+// Add function combine two number. See also https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
